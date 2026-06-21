6aaf2067bd4e3cccf46a4b9b0bcefdf66989c753
Dilshad Rafi Shajli <dilshad.shajli@gmail.com>
2026-06-21 18:20:19 +0600

    dev allowed origins added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://147.93.103.113", "http://localhost"],
 };
 
 export default nextConfig;
