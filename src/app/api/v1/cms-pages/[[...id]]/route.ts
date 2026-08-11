44c245993fc3d0457dd09c40c023ffe4a6667255
Md Dip <mddip1061@gmail.com>
2026-08-11 11:14:06 +0600

    feat(cms): add cms-pages API route

---
+++ src/app/api/v1/cms-pages/[[...id]]/route.ts
@@ -0,0 +1,4 @@
+import { createResourceRoute } from "@/server/resources/createResourceRoute";
+   import { cmsPagesResource } from "@/server/resources/cmsPages";
+
+   export const { GET, POST, PATCH, DELETE } = createResourceRoute(cmsPagesResource);
