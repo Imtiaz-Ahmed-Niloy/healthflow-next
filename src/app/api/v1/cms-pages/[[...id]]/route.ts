9d8b0d9f790662be26a99da1a46a79b9f3e0934e
Md Dip <mddip1061@gmail.com>
2026-08-11 16:54:20 +0600

    style(cms): fix indentation on cms-pages route

--- src/app/api/v1/cms-pages/[[...id]]/route.ts
+++ src/app/api/v1/cms-pages/[[...id]]/route.ts
@@ -1,4 +1,5 @@
 import { createResourceRoute } from "@/server/resources/createResourceRoute";
-   import { cmsPagesResource } from "@/server/resources/cmsPages";
+import { cmsPagesResource } from "@/server/resources/cmsPages";
 
-   export const { GET, POST, PATCH, DELETE } = createResourceRoute(cmsPagesResource);
+export const { GET, POST, PATCH, DELETE } =
+  createResourceRoute(cmsPagesResource);
